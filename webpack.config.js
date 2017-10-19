a0a49941ab173f347d996231db241339d662bf2d
Abraham Williams <4braham@gmail.com>
2017-10-19 11:55:43 -0500

    Include hash in output filenames

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     extensions: [ ".tsx", ".ts", ".js" ]
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[hash].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
